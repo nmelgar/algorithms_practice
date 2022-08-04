fac3de160474ab3e91c03d6ab0824127ab6d979b
nmelgar <nmelgar16013@gmail.com>
2022-08-04 12:15:46 -0500

    Done w/grading students.

--- 8_grading_students.py
+++ 8_grading_students.py
@@ -19,7 +19,38 @@ Examples:
 
 
 def main():
-    print("Hello World")
+    grades = [73, 67, 38, 33]
+    gradingStudents(grades)
+
+
+def gradingStudents(grades):
+    MULTIPLE = 5
+    for grade in grades:
+        index = grades.index(grade)
+        missing = grade % 5
+        if missing >= 3:
+            next_multiple = MULTIPLE - missing
+            new_grade = grade + next_multiple
+            if new_grade >= 40:
+                grades.pop(index)
+                grades.insert(index, new_grade)
+            else:
+                pass
+        elif missing < 3:
+            pass
+    return grades
+
+    # for grade in grades:
+    #     missing = grade % 5
+    #     if missing >= 3:
+    #         next_multiple = MULTIPLE - missing
+    #         new_grade = grade + next_multiple
+    #         if new_grade >= 40:
+    #             print(new_grade)
+    #         else:
+    #             print(grade)
+    #     elif missing < 3:
+    #         print(grade)
 
 
 if __name__ == "__main__":
